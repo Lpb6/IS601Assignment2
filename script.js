eb61b5f9597a2c73286c2a0512820f108568e64f
Liam B <lpb7@njit.edu>
2024-03-14 20:05:35 -0400

    added q1 html and updated q1 js function

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 // Question 1 - vowel and consonant counter
-function countChars(str) {
-  let vowels = 0;
-  let spaces = 0;
-  let consonants = 0;
-  const vowArray = ['a', 'e', 'i', 'o', 'u'];
-  str = str.toLowerCase().split('');
-  console.log(str);
-  for (const i in str) {
-    if (vowArray.includes(str[i])) {
-      vowels++;
-    } else if (str[i] === ' ') {
-      spaces++;
-    } else {
-      consonants++;
+function countChars() {
+  let str = document.querySelector('#stringInput').value;
+  if (str) {
+    let vowels = 0;
+    let spaces = 0;
+    let consonants = 0;
+    const vowArray = ['a', 'e', 'i', 'o', 'u'];
+    if (document.querySelector('#include-y').checked === true) {
+      vowArray.push('y');
     }
+    str = str.toLowerCase().split('');
+    console.log(str);
+    for (const i in str) {
+      if (vowArray.includes(str[i])) {
+        vowels++;
+      } else if (str[i] === ' ') {
+        spaces++;
+      } else {
+        consonants++;
+      }
+    }
+    document.querySelector('#vowelCount').textContent = vowels;
+    document.querySelector('#consonantCount').textContent = consonants;
   }
-  console.log(vowels);
-  console.log(spaces);
-  console.log(consonants);
 }
 countChars('Testing');
 
@@ -32,7 +37,7 @@ function palindromeIdentifier() {
       reversedNum = reversedNum * 10 + (tempNum % 10);
       tempNum = Math.floor(tempNum / 10);
     }
-    if (inputNum === reversedNum) {
+    if (inputNum == reversedNum) {
       document.querySelector('#palindromeResult').textContent =
         'This is a palindrome!';
     } else {
@@ -51,8 +56,9 @@ function tipCalc() {
   const gratuityRate = document.querySelector('#gratuityEntry').value;
   const gratuity = (gratuityRate / 100) * subtotal;
   const total = gratuity + subtotal * 1;
+
   subtotal = subtotal * 1 + 0;
-  document.querySelector('#subtotal-display').textContent = subtotal;
-  document.querySelector('#gratuity-display').textContent = gratuity;
-  document.querySelector('#total-display').textContent = total;
+  document.querySelector('#subtotal-display').textContent = subtotal.toFixed(2);
+  document.querySelector('#gratuity-display').textContent = gratuity.toFixed(2);
+  document.querySelector('#total-display').textContent = total.toFixed(2);
 }
